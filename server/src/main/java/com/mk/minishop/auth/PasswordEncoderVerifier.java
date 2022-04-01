d43a84737e03fdeafbe0dc3d99eca5ce1203d50f
mkmiecik <mich.kmiecik@gmail.com>
2022-04-01 10:46:04 +0200

    Repackaging

--- server/src/main/java/com/mk/minishop/auth/PasswordEncoderVerifier.java
+++
@@ -1,9 +0,0 @@
-package com.mk.minishop.auth;
-
-public interface PasswordEncoderVerifier {
-
-    String encode(CharSequence toEncode);
-
-    boolean matches(CharSequence rawPass, Password encodedPass);
-
-}
--- server/src/main/java/com/mk/minishop/auth/UserContextProvider.java
+++
@@ -1,7 +0,0 @@
-package com.mk.minishop.auth;
-
-public interface UserContextProvider {
-
-    AuthContext userContext();
-
-}
